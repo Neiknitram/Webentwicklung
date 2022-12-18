4fa7973775e8d64377f8f88ef51a790c73929831
Neiknitram <22478352+Neiknitram@users.noreply.github.com>
2022-12-18 16:36:08 +0100

    Bootstrap-Table hinzugefügt.

--- projects.php
+++ projects.php
@@ -5,11 +5,10 @@
     <title>Aufgabenplaner | Projekte</title>
 
     <!-- Bootstrap CSS + Bootsrap Table + Font-Awesome + Stylesheet -->
+    <!-- Bootstrap CSS + Bootsrap Table + Font-Awesome + Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.21.2/bootstrap-table.min.css"
-          integrity="sha512-nR/UyMwqN3G7hUXj555TPsmLNyBHbTuJal56HT4p1iYzJ6wdI8CFQfkHe6POuosBt7FC2dsrhBEGM71TCJyxXQ=="
-          crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.2/dist/bootstrap-table.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
           integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
@@ -85,8 +84,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
         crossorigin="anonymous"></script>
-<script integrity="sha512-Pzu+rRJHcBZGogD+WpK3kZVU5aKuL0W3hVYLePrsa1nL9Kdqch3+7tiSkg/ziHJOx5lTtA20w86zw1yGaF6Scw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://unpkg.com/bootstrap-table@1.21.2/dist/bootstrap-table.min.js"></script>
 
 </body>
 </html>
