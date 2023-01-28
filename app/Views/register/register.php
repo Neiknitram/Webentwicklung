6d99671736450bd3641b1b1e02717c35f8c79539
Neiknitram <22478352+Neiknitram@users.noreply.github.com>
2023-01-28 17:40:34 +0100

    Responsive layout

--- app/Views/register/register.php
+++ app/Views/register/register.php
@@ -8,7 +8,7 @@
     <div class="row">
 
         <!-- Register-Form -->
-        <div class="col-12 col-lg-6 mx-auto">
+        <div class="col-12 col-lg-6 mt-4 mx-auto">
             <div class="row">
                 <h4 class="text-center">Registrierung zur Zeit nicht möglich.</h4>
                 <p class="text-center">Der Registrierungsvorgang wurde technisch noch nicht umgesetzt.
